ed5ab0e3ce838ecb989d8ec706539a823d45cec9
João Rocha <joao@legartis.ai>
2020-12-13 18:04:23 +0000

    Altar Challenge: created Angular Material module

---
+++ src/app/shared/modules/material.module.ts
@@ -0,0 +1,15 @@
+import { MatGridListModule } from '@angular/material/grid-list';
+
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [
+    MatGridListModule,
+  ],
+  exports: [
+    MatGridListModule,
+  ]
+})
+
+export class MaterialModule {
+}
