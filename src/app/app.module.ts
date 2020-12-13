82f935c56eebf712d7d58d52a357af3540b652c5
João Rocha <joao@legartis.ai>
2020-12-13 18:04:44 +0000

    Altar Challenge: created shared module and added it to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GeneratorPageComponent } from './pages/generator-page/generator-page.component';
+import { Error404PageComponent } from './pages/error404-page/error404-page.component';
+import { PaymentsPageComponent } from './pages/payments-page/payments-page.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
+    SharedModule,
     AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    GeneratorPageComponent,
+    Error404PageComponent,
+    PaymentsPageComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
