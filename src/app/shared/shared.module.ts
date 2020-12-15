cbc72e42197295879aa2071211548d68d7bd0adf
João Rocha <joao@legartis.ai>
2020-12-15 16:45:16 +0000

    Altar Challenge: Created Header component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from './modules/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { LiveCodeComponent } from './components/live-code/live-code.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   imports: [
@@ -11,12 +12,14 @@ import { LiveCodeComponent } from './components/live-code/live-code.component';
     FlexLayoutModule
   ],
   declarations: [
-    LiveCodeComponent
+    LiveCodeComponent,
+    HeaderComponent
   ],
   exports: [
     MaterialModule,
     FlexLayoutModule,
-    LiveCodeComponent
+    LiveCodeComponent,
+    HeaderComponent
   ]
 })
 export class SharedModule {
